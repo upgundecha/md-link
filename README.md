a45777c9ea588792b7cff0ba71af6e6a5a2cfaeb
Unmesh Gundecha <upgundecha@gmail.com>
2021-01-08 17:56:02 +0800

    Inital Commit

---
+++ README.md
@@ -0,0 +1,9 @@
+# chrome-md-link-generator
+Chrome extension to generate inline Markdown link from the selected text and current url of the page
+
+The extension will generate the link and copy it to clipboard
+
+---
+### How to use it?
+
+[![How to use Markdown Link Generator](https://img.youtube.com/vi/9HS8XoBnn5o/0.jpg)](https://www.youtube.com/watch?v=9HS8XoBnn5o)
---
+++ main.js
@@ -0,0 +1,24 @@
+function generateMdLink(info, tab) {
+  return function (info, tab) {
+    var selection = info.selectionText;
+    var mdLink = `[${selection}](${tab.url})`
+    copyTextToClipboard(mdLink)
+  }
+}
+
+chrome.contextMenus.create ({
+  "title": "Copy Markdown Link",
+  "type": "normal",
+  "contexts": ["selection"],
+  "onclick": generateMdLink ()
+});
+
+//source - https://www.30secondsofcode.org/blog/s/copy-text-to-clipboard-with-javascript
+function copyTextToClipboard(text) {
+  var copyFrom = document.createElement("textarea");
+  copyFrom.textContent = text;
+  document.body.appendChild(copyFrom);
+  copyFrom.select();
+  document.execCommand('copy');
+  document.body.removeChild(copyFrom);
+}
